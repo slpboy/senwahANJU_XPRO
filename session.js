75ec9a755f57d785503de63bc0ab0765780076dd
slpboy <binancesenura@gmail.com>
2025-04-27 22:41:07 +0530

    Update session.js

--- session.js
+++ session.js
@@ -40,10 +40,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "your session id",
-    GITHUB_TOKEN: process.env.GITHUB_TOKEN || "github token without ghp_",
-    GITHUB_USERNAME: process.env.GITHUB_USERNAME || "github username",
-    SUDO: process.env.SUDO ? process.env.SUDO.split(",") : ["94717775628", "94758775628"],
+    SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~zY9URQiY#tvdhfw2LwaCCJLryopYH8sd3Ys_fs3XuVNnaxda3prw",
+    GITHUB_TOKEN: process.env.GITHUB_TOKEN || "11BR5MMGY0ig8ifPqJKcjb_vay7QlbZZDyvSXW7NeyDe6PPuorWHGqkdgULs104MoDDFCYCZJ3x2qlw0k9",
+    GITHUB_USERNAME: process.env.GITHUB_USERNAME || "slpboy",
+    SUDO: process.env.SUDO ? process.env.SUDO.split(",") : ["94784504579"],
     PORT: process.env.PORT || "8000",
     SESSION_DIR: process.env.SESSION_DIR || "session",
     FOOTER: process.env.FOOTER || "> QUEEN ANJU XPRO\n> Developed by Mr. Rashmika\n> GitHub Repository: github.com/Mrrashmika/QUEEN_ANJU_xPro\n\ud83c\udf1f Premium Version | Advanced Features | Secure & Reliable \ud83c\udf1f\n\u00a9 2025 QUEEN ANJU XPRO. All rights reserved." 
